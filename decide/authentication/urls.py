6f2876bea49b079559a9d471724131dfd75ab824
thealean28 <alejandrogallardopelayo@gmail.com>
2023-11-12 20:15:24 +0100

    feat: register and login user views
    cuerpo

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import GetUserView, LogoutView, RegisterView
+from .views import GetUserView, LogoutView, RegisterViewAPI, LoginView, RegisterView, logout_view
 
 
 urlpatterns = [
+    path('login-view/',LoginView.as_view(), name='login'),
     path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
+    path('register-api/', RegisterViewAPI.as_view()),
     path('register/', RegisterView.as_view()),
+    path('logout-view/',logout_view)
 ]
