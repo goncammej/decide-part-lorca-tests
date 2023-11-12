0497a5454e9a8ad711da6360bb6ac8d88ac4f789
thealean28 <alejandrogallardopelayo@gmail.com>
2023-11-12 20:15:24 +0100

    feat: register and login user views
    cuerpo

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -180,3 +180,10 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="decidepartlorca2324@gmail.com"
+EMAIL_HOST_PASSWORD="minv wukr swha dxnv" 
