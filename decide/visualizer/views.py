ca0cffb72e43870fcd3313230ea040acd1ba74c6
alejandromd <aleprieto02@gmail.com>
2023-11-27 21:53:06 +0100

    feat: Implemented visualizer for comment vote #140

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -1,9 +1,12 @@
 import json
-from django.views.generic import TemplateView
+
 from django.conf import settings
 from django.http import Http404
+from django.views.generic import TemplateView
 
 from base import mods
+from census.models import Census
+from store.models import Vote
 
 
 class VisualizerView(TemplateView):
@@ -11,12 +14,26 @@ class VisualizerView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        vid = kwargs.get('voting_id', 0)
-
+        voting_id = kwargs.get('voting_id', 0)
+    
         try:
-            r = mods.get('voting', params={'id': vid})
-            context['voting'] = json.dumps(r[0])
+            voting = mods.get('voting', params={'id': voting_id})
+            context['voting'] = json.dumps(voting[0])
+            num_census=0
+            num_votes=0
+            participation="-"
+            print(context['voting'])
+            if voting[0].get('start_date'):
+                num_census = Census.objects.filter(voting_id=voting_id).count()
+                num_votes = Vote.objects.filter(voting_id=voting_id).count()
+
+                if num_census != 0:
+                    participation = str(round((num_votes*100)/num_census,2))+'%'
+
+            realtimedata = {'num_census':num_census, 'num_votes':num_votes, 'participation':participation}
+            context['realtimedata'] = realtimedata
+            
         except:
             raise Http404
-
+            
         return context
