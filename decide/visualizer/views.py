05cd96c5d62bb9318a199bdce80c8b5c85ed304b
alejandromd <aleprieto02@gmail.com>
2023-11-28 17:44:02 +0100

    feat: Implemented visualizer for mult. choice #165

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -11,7 +11,7 @@ from store.models import Vote
 
 class VisualizerView(TemplateView):
     template_name = 'visualizer/visualizer.html'
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         voting_id = kwargs.get('voting_id', 0)
@@ -22,15 +22,15 @@ class VisualizerView(TemplateView):
             num_census=0
             num_votes=0
             participation="-"
-
+            print(context['voting'])
             if voting[0].get('start_date'):
                 num_census = Census.objects.filter(voting_id=voting_id).count()
-                num_votes = Vote.objects.filter(voting_id=voting_id).count()
-
+                num_votes = Vote.objects.filter(voting_id=voting_id)
+                num_voters= len(set(vote.voter_id for vote in num_votes))
                 if num_census != 0:
-                    participation = str(round((num_votes*100)/num_census,2))+'%'
+                    participation = str(round((num_voters*100)/num_census,2))+'%'
 
-            realtimedata = {'num_census':num_census, 'num_votes':num_votes, 'participation':participation}
+            realtimedata = {'num_census':num_census, 'num_votes':num_voters, 'participation':participation}
             context['realtimedata'] = realtimedata
             
         except:
