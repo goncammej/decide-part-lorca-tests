7e22b1bab856129da273ceaa154b55bea8f18d28
goncammej <goncammej@alum.us.es>
2023-12-05 19:58:41 +0100

    conf: Add docker deployment #154
    Adds the necessary configuration to launch the application with redis
    and celery services. It also adds the launching of specific branches.

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -178,6 +178,15 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+CELERY_BROKER_URL = os.environ.get("BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("RESULT_BACKEND", "redis://localhost:6379/0")
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+CELERY_TASK_DEFAULT_QUEUE = 'default'
+
 try:
     from local_settings import *
 except ImportError:
