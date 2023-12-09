102d2ef87e49dd68ca05fb718ac68fe921c14502
Álvaro González <alvgonfri@alum.us.es>
2023-12-09 02:36:06 +0100

    test: Added classic voting test #60
    I have tested the creation of a classic voting from the configurator

--- decide/configurator/tests.py
+++ decide/configurator/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from base.tests import BaseTestCase
+from django.urls import reverse
+from django.contrib.messages import get_messages
+from .forms import ClassicForm
 
-# Create your tests here.
+
+class CreateClassicViewTest(BaseTestCase):
+    def test_get_request(self):
+        response = self.client.get(reverse("create_classic"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "configurator/create_classic.html")
+        self.assertIsInstance(response.context["form"], ClassicForm)
+
+    def test_post_request_valid_form(self):
+        data = {
+            "name": "Test Voting",
+            "desc": "This is a test voting",
+            "question_desc": "This is a test question",
+            "option1": "Option 1",
+            "option2": "Option 2",
+            "more_options": "Option 3\nOption 4",
+        }
+        response = self.client.post(reverse("create_classic"), data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("manage_census"))
+
+        self.assertEqual(self.client.session["voting_id"], 1)
+
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "Classic voting created successfully!")
+
+    def test_post_request_invalid_form(self):
+        data = {
+            "name": "",  # Empty name
+            "desc": "This is a test voting",
+            "question_desc": "This is a test question",
+            "option1": "Option 1",
+            "option2": "Option 2",
+            "more_options": "Option 3\nOption 4",
+        }
+        response = self.client.post(reverse("create_classic"), data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "configurator/create_classic.html")
+
+        form = response.context["form"]
+
+        self.assertFalse(form.is_valid())
