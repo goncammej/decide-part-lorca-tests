36b9ac8e11b829582cd017bae03bc7184ef4db7a
[[alblopnov]] <[[a.lopezbenjumea@gmail.com]]>
2023-12-14 22:43:52 +0100

    fix: Keep authorization - Booth#297

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -13,6 +13,7 @@ class BoothView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
+        context['user'] = self.request.user
 
         try:
             r = mods.get('voting', params={'id': vid})
